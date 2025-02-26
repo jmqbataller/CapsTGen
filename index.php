8fdd9237771afeb4c6768da944f0dc74caa97278
John Mark <88698057+jmqbataller@users.noreply.github.com>
2025-02-26 21:02:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<head>
-    <title>Capstone Idea Generator</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-</head>
-
-    <h1>Capstone Idea Generator</h1>
-    <button onclick="generateCapstone()">Generate Capstone</button> <br>
-
-    <div id="capstone-display"></div>
-
-
-
-    <footer>
-    Made by <a href="https://github.com/jmqbataller" target="_blank" rel="noopener noreferrer">JMDev</a> ♡
-</footer>
-
-    <script src="script.js"></script>
-
-</body>
-</html>
+<?php
+echo "Hello from PHP!"; 
+?>
